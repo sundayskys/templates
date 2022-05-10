af3965e063d39d3be534524dcc43800fffe7a8eb
yanzhao <1770285990@qq.com>
2022-05-10 15:41:44 +0800

    [新增功能](master): init
    🔰 初始化

---
+++ springboot-helloworld/src/main/java/com/controoler/IndexController.java
@@ -0,0 +1,19 @@
+package com.controoler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author YanZhao
+ * @description 初始化启动
+ * @date 2022年05月10日 15:12
+ */
+
+@RestController
+public class IndexController {
+
+    @GetMapping("index")
+    public String index(){
+        return "Hello-World";
+    }
+}
